7a55054019b8c1c4efe6e810976040bddc3b13ca
Marcus Wendel <BabyMac@Marcuss-MacBook-Pro.local>
2018-01-10 14:55:11 +0100

    added first lesson

--- react-school/src/App.js
+++ react-school/src/App.js
@@ -1,18 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import banner from './image.png';
 import './App.css';
+
+/////////////////////////
+// Imports needed to develop, more can be added here if needed.
+/////////////////////////
+
+
+//import YoutubeController from './youtube-list-example/youtube-list-example';
+//import HelloWorldExample from './helloworld-example/helloworld-example';
+
+////////////////////////
+// Comment out the components your not using for easier developing.
+// And dont forget to remove the JSX component tag in the return.
+///////////////////////
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+        <img src={banner} className="banner" alt="banner" />
+        <h1 className="App-title">Welcome to Purplescouts React school.</h1>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To start your development simply add your Component's to the src directory. <br/>
+          To find the exercises and homework, read the README file on github or in the directory
         </p>
+        <p className="emoji">
+          (•_•) ( •_•)>⌐■-■ (⌐■_■)
+        </p>
+
+        {/*<HelloWorldExample />*/}
+        {/*<YoutubeController />*/}
       </div>
     );
   }
