9c561f729b58e31d55ea0a5e0b5182f796f028d8
Babymac <marcus.wendel@purplescout.se>
2018-01-24 10:54:22 +0100

    minor changes

--- app/src/content.js
+++ app/src/content.js
@@ -4,6 +4,11 @@ import { HashRouter as Router, Route } from 'react-router-dom';
 import Home from './home';
 import Week1 from './week1';
 import Week2 from './week2';
+// import Week3 from './week3';
+// import Week4 from './week4';
+// import Week5 from './week5';
+// import Week6 from './week6';
+// import Week7 from './week7';
 
 const Content = () => (
 	<Router>
@@ -11,7 +16,11 @@ const Content = () => (
 			<Route exact path="/" component={Home} />
 			<Route path="/week1" component={Week1} />
 			<Route path="/week2" component={Week2} />
-			{/* <Route path="/week2" component={Week2} /> */}
+			{/* <Route path="/week3" component={Week3} /> */}
+			{/* <Route path="/week4" component={Week4} /> */}
+			{/* <Route path="/week5" component={Week5} /> */}
+			{/* <Route path="/week6" component={Week6} /> */}
+			{/* <Route path="/week7" component={Week7} /> */}
 		</div>
 	</Router>
 );
